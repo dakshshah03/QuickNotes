5aeab36cc19c4721dff40ab76670a3d9d5eb4142
Daksh Shah <daksh.shah15@gmail.com>
2025-07-22 22:53:15 -0700

    started vector store

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 MVP features
-    - upload pdf
-        - save notebook to backend cache
-        - parse notebook
-        - clean parsed words
-        - stateless vector db: faiss
+- upload pdf
+    - save pdf to backend cache
+    - works with pdfs that have text layers
+    - parse pdf
+- vector db: chromadb
+    
 Planned features:
-    - retain pdf
-    - retain vector store
+- retain pdf
+- retain vector store
+- work with pdf that may not contain text (tesseract fallback?)
+- login
+- separate notebooks (and pdfs in each notebook that can be enabled or disabled)
+
+Current Goals:
+- get embedding to work with single pdfs
+- implement fastapi endpoints
+    - PUT pdf
+    
--- src/rag/pdf_parser.py
+++ src/rag/pdf_parser.py
@@ -2,26 +2,25 @@ import pymupdf4llm
 from langchain.text_splitter import MarkdownTextSplitter
 from langchain_core.documents import Document
 
-def pdf_to_chunk(pdf_path: str, chunk_size: int = 500, chunk_overlap: int = 50) -> list:
+def pdf_to_chunk(file_name: str, pdf_path: str, chunk_size: int = 500, chunk_overlap: int = 50) -> list[Document]:
     """
-    Parses a pdf, extracts text, then returns pdf contents as a string
+    Parses a pdf, extracts text, then returns pdf contents as a list of langchain Documents
     """
     pdf_md = pymupdf4llm.to_markdown(pdf_path)
     
-    # splitter = MarkdownTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     text_splitter = MarkdownTextSplitter.from_tiktoken_encoder(
         encoding_name="cl100k_base",
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
     )
     
-    metadata = {"source": pdf_path, "format": "markdown"}
+    metadata = {"source": pdf_path,"file_name": file_name,"format": "markdown"}
     
     
     md_doc = Document(page_content=pdf_md, metadata=metadata)
     chunks = text_splitter.split_documents([md_doc])
     
     for i, chunk in enumerate(chunks):
-        chunk.metadata["chunk_id"] = i
+        chunk.id = i
     
     return chunks
