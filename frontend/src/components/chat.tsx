001f2fde0774f80431f21e53aa6e67e11a5d784d
Daksh Shah <daksh.shah15@gmail.com>
2025-07-26 19:22:28 -0700

    init react project

---
+++ frontend/src/components/chat.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+interface Message {
+    id: string;
+    sender: 'user' | 'llm';
+    content: string;
+    isCurrentUser: boolean;
+}
+
+// list of messages
+interface MessageWindow {
+    messages: Message[];
+}
+
+
+
+
+
