a6e22b32e3739a0097500067d2eda2f83fb250e3
Daksh Shah <44329654+dakshshah03@users.noreply.github.com>
2025-08-12 20:18:28 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   apps : [{
     name   : "backend-api",
-    script : "uv run uvicorn",
-    args   : "main:app",
-    cwd    : "/root/websites/QuickNotes/backend",
+    script : "uv",
+    args   : "run uvicorn main:app",
+    cwd    : "/root/websites/QuickNotes/backend/src"
   },{
     name   : "frontend",
-    script : "npm run",
-    args   : "start",
-    cwd    : "/root/websites/QuickNotes/frontend",
+    script : "npm",
+    args   : "run start",
+    cwd    : "/root/websites/QuickNotes/frontend"
   },{
     name   : "db",
-    script : "docker compose up",
-    args   : "-f",
-    cwd    : "/root/websites/QuickNotes/docker-compose.yaml",
+    script : "docker compose -f /root/websites/QuickNotes/docker-compose.yaml up"
   }]
 }
+
